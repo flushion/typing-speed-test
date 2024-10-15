6a236ab08323e6ba3c84ade5d9d2b7428d1a3966
Illia Linkevych (flushion) <66466625+flushion@users.noreply.github.com>
2024-10-15 16:40:21 +0200

    Update addZero.ts -> English translation

--- src/utils/addZero.ts
+++ src/utils/addZero.ts
@@ -1,7 +1,7 @@
 /**
- * Преобразует число в строку с ведущим нулем, если число меньше 10.
+ * Converts a number to a string with a leading zero if the number is less than 10.
  *
- * @param {number} n - Число для преобразования.
- * @return {string} Преобразованная строка.
+ * @param {number} n - The number to convert.
+ * @return {string} The converted string.
  */
 export const addZero = (n: number): string => (n < 10 ? `0${n}` : n).toString();
